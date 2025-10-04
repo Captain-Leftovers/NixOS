cad0d8cb57b5d06be67a50f9a60d376bd5404c83
BeeOnDWeb <beeondweb@gmail.com>
2025-10-04 19:43:10 +1000

    adding mkdefault to rofi heme
    Signed-off-by: BeeOnDWeb <beeondweb@gmail.com>

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -4,11 +4,13 @@
     enable = true;
     settings = {
       general = {
-        disable_loading_bar = true;
-        grace = 10;
-        hide_cursor = true;
-        no_fade_in = false;
+        disable_loading_bar = lib.mkDefault true;
+        grace = lib.mkDefault 10;
+        hide_cursor = lib.mkDefault true;
+        no_fade_in = lib.mkDefault false;
       };
+
+      # You already used mkDefault here — keep it:
       background = lib.mkDefault [
         {
           path = "/home/${username}/Pictures/Wallpapers/16.png";
@@ -16,19 +18,23 @@
           blur_size = 8;
         }
       ];
-      image = [
+
+      # If Stylix also sets images, make this mkDefault too:
+      image = lib.mkDefault [
         {
           path = "/home/${username}/black-don-os/img/berserk.png";
           size = 150;
           border_size = 4;
           border_color = lib.mkDefault "rgb(0C96F9)";
-          rounding = -1; # Negative means circle
+          rounding = -1;
           position = "0, 200";
           halign = "center";
           valign = "center";
         }
       ];
-      input-field = [
+
+      # This line was the main offender — make the WHOLE list mkDefault:
+      input-field = lib.mkDefault [
         {
           size = "200, 50";
           position = "0, -80";
@@ -38,9 +44,9 @@
           font_color = lib.mkDefault "rgb(CFE6F4)";
           inner_color = lib.mkDefault "rgb(657DC2)";
           outer_color = lib.mkDefault "rgb(0D0E15)";
-          outline_thickness = 5;
+          outline_thickness = lib.mkDefault 5;
           placeholder_text = "Password...";
-          shadow_passes = 2;
+          shadow_passes = lib.mkDefault 2;
         }
       ];
     };
