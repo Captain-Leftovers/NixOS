25278c16007db526a1e404321633ff00ec208c34
BeeOnDWeb <beeondweb@gmail.com>
2025-10-05 23:41:12 +1100

    trying to remo zen flatpak [27;2;13~removed BROWSER env
    Signed-off-by: BeeOnDWeb <beeondweb@gmail.com>

--- modules/home/environment.nix
+++ modules/home/environment.nix
@@ -1,6 +1,7 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.sessionVariables = {
     CHROME_EXECUTABLE = "/run/current-system/sw/bin/google-chrome-stable";
-    BROWSER = "flatpak run io.github.zen_browser.zen";
+    # BROWSER = "flatpak run io.github.zen_browser.zen";
   };
 }
