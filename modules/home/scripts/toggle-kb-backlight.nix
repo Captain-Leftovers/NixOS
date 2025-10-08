9e4378ec04cf613b7dcff368b6eb00b4725615f5
BeeOnDWeb <beeondweb@gmail.com>
2025-10-08 23:11:55 +1100

    kb backlight script
    Signed-off-by: BeeOnDWeb <beeondweb@gmail.com>

---
+++ modules/home/scripts/toggle-kb-backlight.nix
@@ -0,0 +1,17 @@
+{ pkgs }:
+pkgs.writeShellScriptBin "toggle-kb-backlight" ''
+
+  LED_PATH="/sys/class/leds/tpacpi::kbd_backlight/brightness"
+  MAX=2
+
+  cur=$(cat "$LED_PATH")
+  next=$(( (cur + 1) % (MAX + 1) ))
+
+  echo $next > "$LED_PATH"
+
+  case "$next" in
+    0) notify-send "💡 Keyboard Backlight" "Turned OFF" ;;
+    1) notify-send "💡 Keyboard Backlight" "Set to LOW" ;;
+    2) notify-send "💡 Keyboard Backlight" "Set to HIGH" ;;
+  esac
+''
